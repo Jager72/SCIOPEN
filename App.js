b2ee7230b3caef045426f448acf031386a823ceb
KSmalu <krz.smal@gmail.com>
2021-11-04 17:58:48 +0100

    Initial login commit

--- App.js
+++ App.js
@@ -4,12 +4,12 @@ import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 import ConfigureOrder from './modules/ConfigureOrder'
 import MenuBar from './modules/MenuBar'
+import Login from './modules/Login'
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-      <MenuBar CurrentPageTitle={"TOSTY"}/>
-      <ConfigureOrder />
+      <Login navigation={"DUPA"}/>
     </SafeAreaView>
   );
 }
@@ -17,5 +17,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     paddingTop:40,
+    flex:1,
   },
 });
