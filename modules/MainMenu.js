9dbaaa2c4d54f7e195b09d92b2d82f3d28a10e15
4RCZ1 <49472612+4RCZ1@users.noreply.github.com>
2021-11-19 09:59:09 +0100

    better styling

--- modules/MainMenu.js
+++ modules/MainMenu.js
@@ -1,21 +1,34 @@
 import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
-import { color } from '../helpers/styles';
+import {StyleSheet, Text, View} from 'react-native';
+import {color} from '../helpers/styles';
 import Toast from '../assets/Toast'
 import Door from '../assets/Door'
 import User from '../assets/User';
 import Room from '../assets/Room';
-const MainMenu = ({CurrentPageTitle}) => {
-    return(
+
+const MainMenu = () => {
+    return (
         <View style={styles.container}>
-                <View style={styles.titlewraper}>
+            <View style={styles.titlewraper}>
                 <Text style={styles.textHeader}>Menu główne</Text>
-                </View>
+            </View>
             <View style={styles.Background}>
-                <View style={styles.block}><Toast width="60%" height="60%"></Toast><Text style={styles.textHeader}>Tosty</Text></View>
-                <View style={styles.block}><User width="60%" height="60%"></User><Text style={styles.textHeader}>Profil</Text></View>
-                <View style={styles.block}><Room width="60%" height="60%"></Room><Text style={styles.textHeader}>Pokoje</Text></View>
-                <View style={styles.block}><Door width="60%" height="60%" ></Door><Text style={styles.textHeader}>Wyjście</Text></View>
+                <View style={styles.block}>
+                    <Toast width="60%" height="60%"/>
+                    <Text style={styles.textHeader}>Tosty</Text>
+                </View>
+                <View style={styles.block}>
+                    <User width="60%" height="60%"/>
+                    <Text style={styles.textHeader}>Profil</Text>
+                </View>
+                <View style={styles.block}>
+                    <Room width="60%" height="60%"/>
+                    <Text style={styles.textHeader}>Pokoje</Text>
+                </View>
+                <View style={styles.block}>
+                    <Door width="60%" height="60%"/>
+                    <Text style={styles.textHeader}>Wyjście</Text>
+                </View>
             </View>
         </View>
     )
@@ -27,13 +40,13 @@ const styles = StyleSheet.create({
     Background: {
         flex: 1,
         flexWrap: "wrap",
-        backgroundColor: color.secondaryColor ,
+        backgroundColor: color.secondaryColor,
         alignContent: "space-around",
     },
-    block:{
+    block: {
         width: "45%",
         height: "40%",
-        marginTop:"2%",
+        marginTop: "2%",
         backgroundColor: color.primaryColor,
         borderRadius: 30,
         justifyContent: 'center',
@@ -44,15 +57,15 @@ const styles = StyleSheet.create({
         textAlign: "center",
         fontWeight: "bold",
         fontSize: 30,
-      },
-      titlewraper:{
+    },
+    titlewraper: {
         width: "100%",
         height: "10%",
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: color.highlightColor,
-      },
-      container:{
+    },
+    container: {
         flex: 1,
-      }
+    }
 });
