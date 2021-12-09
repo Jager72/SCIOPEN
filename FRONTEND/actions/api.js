045cd13a65b612aff6aeb146bccd3088153c4aeb
4RCZ1 <49472612+4RCZ1@users.noreply.github.com>
2021-12-09 12:11:08 +0100

    ready for backend

---
+++ FRONTEND/actions/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const baseUrl='https://localhost:5001/';
+
+export default {
+    orders(url=baseUrl+'Orders/'){
+        return{
+            fetchAll:()=>axios.get(url),
+            add:(order)=>axios.post(url,order),
+            edit:(order)=>axios.put(url,order),
+            delete:(id)=>axios.delete(url+id)
+        }
+    },
+    user(url=baseUrl+'user/'){
+        return{
+            login: (login,pin) => axios.get(url+login+"/"+pin,{withCredentials:true})
+        }
+    }
+}
