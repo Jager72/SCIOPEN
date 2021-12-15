c052f49ae23197c888e797533e84bb04e449864b
Adam <adampilawski2002@gmail.com>
2021-12-15 23:40:47 +0100

    Added Users Admin Manager

--- FRONTEND/modules/MainMenu.js
+++ FRONTEND/modules/MainMenu.js
@@ -22,10 +22,10 @@ const MainMenu = () => {
                         <Text style={styles.textHeader}>Tosty</Text>
                     </View>
                 </Link>
-                <Link to='/myProfile' style={styles.block}>
+                <Link to='/adminUsers' style={styles.block}>
                     <View style={styles.block2}>
                         <User width="60%" height="60%"/>
-                        <Text style={styles.textHeader}>Profil</Text>
+                        <Text style={styles.textHeader}>Użytkownicy</Text>
                     </View>
                 </Link>
                 <Link to="/classroomManager" style={styles.block}>
