fced1fde11e3fc0128b6627e2c3f5aca6514ecb1
4RCZ1 <marcinowicz1@gmail.com>
2021-12-16 20:48:26 +0100

    redux remaster v2

--- FRONTEND/reducers/rooms.js
+++ FRONTEND/reducers/rooms.js
@@ -1,8 +1,9 @@
-import {ACTION_TYPES} from "../actions/actionTypes";
+import {ACTION_TYPES,ROOM} from "../actions/actionTypes";
 
 const initialState = {
     currentRoom:null,
-    visitedRooms:[]
+    visitedRooms:[],
+    rooms:[]
 };
 
 export const rooms = (state = initialState,action) => {
@@ -10,12 +11,38 @@ export const rooms = (state = initialState,action) => {
         case ACTION_TYPES.SET_CURRENT_ROOM:
             return {
                 currentRoom: action.payload,
-                visitedRooms: state.visitedRooms
+                visitedRooms: state.visitedRooms,
+                rooms: state.rooms
             };
         case ACTION_TYPES.ADD_VISITED_ROOM:
             return {
                 currentRoom: state.currentRoom,
                 visitedRooms: state.visitedRooms.concat(action.payload),
+                rooms: state.rooms
+            };
+        case ACTION_TYPES.FETCH_ALL.concat(ROOM):
+            return {
+                currentRoom: state.currentRoom,
+                visitedRooms: state.visitedRooms,
+                rooms: action.payload
+            };
+        case ACTION_TYPES.CREATE.concat(ROOM):
+            return {
+                currentRoom: state.currentRoom,
+                visitedRooms: state.visitedRooms,
+                rooms: state.rooms.concat(action.payload)
+            };
+        case ACTION_TYPES.UPDATE.concat(ROOM):
+            return {
+                currentRoom: state.currentRoom,
+                visitedRooms: state.visitedRooms,
+                rooms: state.rooms.map(room => room.id === action.payload.id ? action.payload : room)
+            };
+        case ACTION_TYPES.DELETE.concat(ROOM):
+            return {
+                currentRoom: state.currentRoom,
+                visitedRooms: state.visitedRooms,
+                rooms: state.rooms.filter(room => room.id !== action.payload)
             };
         default:
             return state;
